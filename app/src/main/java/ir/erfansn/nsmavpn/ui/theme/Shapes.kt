dbd28c92e1ebe20a31c511b1121bb206dc02ce76
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-10-09 21:04:13 +0330

    Initial commit

---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/theme/Shapes.kt
@@ -0,0 +1,5 @@
+package ir.erfansn.nsmavpn.ui.theme
+
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes()
---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package ir.erfansn.nsmavpn.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun NsmaVpnTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = if (dynamicColor) {
+        val context = LocalContext.current
+        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    } else {
+        if (darkTheme) DarkColorScheme else LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
