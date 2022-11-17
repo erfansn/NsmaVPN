0fdbf3526ae9115e96157834b9ffe12939201f6b
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-11-17 22:49:20 +0330

    Provide VpnProvider Datastore in AppModule

--- app/src/main/java/ir/erfansn/nsmavpn/di/AppModule.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/di/AppModule.kt
@@ -1,15 +1,21 @@
 package ir.erfansn.nsmavpn.di
 
+import android.content.Context
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import ir.erfansn.nsmavpn.data.source.local.datastore.VpnProviderSerializer
 import ir.erfansn.nsmavpn.data.source.remote.DefaultVpnGateMessagesRemoteDataSource
 import ir.erfansn.nsmavpn.data.source.remote.VpnGateMessagesRemoteDataSource
 import ir.erfansn.nsmavpn.data.source.remote.api.GmailApi
 import ir.erfansn.nsmavpn.data.source.remote.api.GmailApiImpl
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,6 +30,14 @@ abstract class AppModule {
     ): VpnGateMessagesRemoteDataSource
 
     companion object {
+
+        @[Provides Singleton]
+        fun providesVpnProviderDataStore(@ApplicationContext context: Context) =
+            DataStoreFactory.create(
+                serializer = VpnProviderSerializer,
+                produceFile = { context.dataStoreFile("vpn_provider") }
+            )
+
         @Provides
         fun providesIoDispatcher() = Dispatchers.IO
     }
