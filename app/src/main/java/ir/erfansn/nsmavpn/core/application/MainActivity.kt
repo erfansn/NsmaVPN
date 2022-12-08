9675007500bce9266f837fb9a3572ac5935de65d
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-12-08 23:06:13 +0330

    Implement SplashScreen

--- app/src/main/java/ir/erfansn/nsmavpn/core/application/MainActivity.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/core/application/MainActivity.kt
@@ -1,6 +1,7 @@
 package ir.erfansn.nsmavpn.core.application
 
 import android.os.Bundle
+import android.window.SplashScreenView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import ir.erfansn.nsmavpn.ui.theme.NsmaVpnTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             NsmaVpnTheme {
