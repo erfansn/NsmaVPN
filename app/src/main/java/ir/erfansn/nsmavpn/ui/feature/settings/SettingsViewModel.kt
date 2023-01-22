b7c6d2ef9baa270ec4815709cfc20d5c74a885fc
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-01-22 19:24:30 +0330

    Add SettingsViewModel

---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/feature/settings/SettingsViewModel.kt
@@ -0,0 +1,36 @@
+package ir.erfansn.nsmavpn.ui.feature.settings
+
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.erfansn.nsmavpn.data.model.ThemeMode
+import ir.erfansn.nsmavpn.data.repository.ConfigurationsRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val configurationsRepository: ConfigurationsRepository,
+) : ViewModel() {
+
+    val uiState: StateFlow<SettingsUiState> = configurationsRepository.configurations.map {
+        SettingsUiState(themeMode = it.themeMode)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = SettingsUiState()
+    )
+
+    fun updateThemeMode(themeMode: ThemeMode) {
+        viewModelScope.launch {
+            configurationsRepository.setThemeMode(themeMode)
+        }
+    }
+}
+
+@Immutable
+data class SettingsUiState(
+    val themeMode: ThemeMode = ThemeMode.SYSTEM
+)
