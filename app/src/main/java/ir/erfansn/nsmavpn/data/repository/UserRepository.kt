807d9130c7c781c5313d7a9a82a918620bf3af32
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-01-25 21:02:19 +0330

    Add a method to get user profile info

--- app/src/main/java/ir/erfansn/nsmavpn/data/repository/UserRepository.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/repository/UserRepository.kt
@@ -1,21 +1,42 @@
 package ir.erfansn.nsmavpn.data.repository
 
+import ir.erfansn.nsmavpn.data.model.Profile
+import ir.erfansn.nsmavpn.data.model.toProfile
 import ir.erfansn.nsmavpn.data.source.local.UserPreferencesLocalDataSource
+import ir.erfansn.nsmavpn.data.source.remote.PersonInfoRemoteDataSource
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val userPreferencesLocalDataSource: UserPreferencesLocalDataSource,
+    private val personInfoRemoteDataSource: PersonInfoRemoteDataSource,
     private val externalScope: CoroutineScope,
 ) {
+    private val mutex = Mutex()
+    private var userProfile: Profile? = null
+
     suspend fun getUserAccountId(): String =
         userPreferencesLocalDataSource.userPreferences.first().emailAddress
 
-    suspend fun saveUserAccountId(id: String) {
+    suspend fun saveUserAccountId(userAccountId: String) {
         externalScope.launch {
-            userPreferencesLocalDataSource.saveUserEmailAddress(address = id)
+            userPreferencesLocalDataSource.saveUserEmailAddress(userAccountId)
         }.join()
+    }
+
+    suspend fun getUserProfile(userAccountId: String): Profile {
+        return externalScope.async {
+            mutex.withLock {
+                userProfile ?: personInfoRemoteDataSource.fetchPublicInfo(userAccountId).toProfile()
+                    .also {
+                        userProfile = it
+                    }
+            }
+        }.await()
     }
 }
