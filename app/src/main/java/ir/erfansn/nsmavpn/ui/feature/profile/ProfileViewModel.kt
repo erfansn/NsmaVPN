bc32b73e58486d01b1f86afda739a734dc6710c6
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-01-27 21:27:31 +0330

    Add ProfileViewModel

---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/feature/profile/ProfileViewModel.kt
@@ -0,0 +1,43 @@
+package ir.erfansn.nsmavpn.ui.feature.profile
+
+import androidx.compose.runtime.Immutable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.erfansn.nsmavpn.data.repository.UserRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+
+    val uiState = userRepository.userProfile.map {
+        ProfileUiState(
+            avatarUrl = it.avatarUrl,
+            emailAddress = it.emailAddress,
+            displayName = it.displayName
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = ProfileUiState()
+    )
+
+    fun signOut() {
+        viewModelScope.launch {
+            userRepository.saveUserProfile(null)
+        }
+    }
+}
+
+@Immutable
+data class ProfileUiState(
+    val avatarUrl: String? = null,
+    val emailAddress: String = "",
+    val displayName: String = ""
+)
