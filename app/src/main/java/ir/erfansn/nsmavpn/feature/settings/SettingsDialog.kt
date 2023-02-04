9c5eaebab944b6b6bc25297e9f6484864188fbac
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-02-04 20:57:52 +0330

    Balance spaces inside dialog body

--- app/src/main/java/ir/erfansn/nsmavpn/feature/settings/SettingsDialog.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/feature/settings/SettingsDialog.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -48,9 +49,14 @@ private fun SettingsDialog(
         },
         text = {
             Divider()
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .wrapContentSize()
+                    .padding(top = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
                 Text(
-                    modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
                     text = stringResource(R.string.theme_mode),
                     style = MaterialTheme.typography.titleMedium,
                 )
@@ -75,7 +81,7 @@ private fun SettingsDialog(
                 Text(
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
-                        .padding(top = 16.dp),
+                        .padding(top = 8.dp),
                     text = stringResource(R.string.app_version, BuildConfig.VERSION_NAME)
                 )
             }
