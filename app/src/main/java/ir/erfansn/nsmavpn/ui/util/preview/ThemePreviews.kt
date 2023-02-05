aacf9bafd9d8ed690cc1e0b7c239b052fcafb843
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-02-05 23:16:51 +0330

    Add ThemePreviews

---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/util/preview/ThemePreviews.kt
@@ -0,0 +1,13 @@
+package ir.erfansn.nsmavpn.ui.util.preview
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true
+)
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+annotation class ThemePreviews
