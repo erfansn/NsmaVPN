0836ac3a1aea8a97d840d3173de3c218fa1bc299
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-02-05 22:58:12 +0330

    Add various devices to preview

--- app/src/main/java/ir/erfansn/nsmavpn/ui/ThemePreviews.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/ThemePreviews.kt
@@ -1,8 +1,45 @@
 package ir.erfansn.nsmavpn.ui
 
 import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(showBackground = true)
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(
+    name = "Phone",
+    group = "Light theme",
+    showBackground = true
+)
+@Preview(
+    name = "Foldable",
+    group = "Light theme",
+    showBackground = true,
+    device = Devices.FOLDABLE
+)
+@Preview(
+    name = "Tablet",
+    group = "Light theme",
+    showBackground = true,
+    device = Devices.TABLET
+)
+
+@Preview(
+    name = "Phone",
+    group = "Dark theme",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "Foldable",
+    group = "Dark theme",
+    showBackground = true,
+    device = Devices.FOLDABLE,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "Tablet",
+    group = "Dark theme",
+    showBackground = true,
+    device = Devices.TABLET,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 annotation class ThemePreviews
