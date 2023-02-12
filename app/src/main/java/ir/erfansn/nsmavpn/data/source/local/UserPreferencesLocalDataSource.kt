6eb2049af18f8049309cb353a6daf55f092e265c
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-02-12 23:08:16 +0330

    Clean up saveUserProfile logic

--- app/src/main/java/ir/erfansn/nsmavpn/data/source/local/UserPreferencesLocalDataSource.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/source/local/UserPreferencesLocalDataSource.kt
@@ -7,11 +7,6 @@ import ir.erfansn.nsmavpn.data.source.local.datastore.*
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-/**
- * Instead of using Impl suffix we use Default prefix because Impl suffix violates DRY principle.
- *
- * [Reference](https://link.medium.com/u5o2xd8Ahxb)
- */
 class DefaultUserPreferencesLocalDataSource @Inject constructor(
     private val dataStore: DataStore<UserPreferences>,
 ) : UserPreferencesLocalDataSource {
@@ -33,25 +28,21 @@ class DefaultUserPreferencesLocalDataSource @Inject constructor(
     override suspend fun saveUserProfile(profile: Profile?) {
         dataStore.updateData {
             it.copy {
-                if (profile != null) {
-                    profileProto = profileProto {
-                        emailAddress = profile.emailAddress
-                        avatarUrl = profile.avatarUrl.orEmpty()
-                        displayName = profile.displayName
-                    }
-                } else {
+                if (profile == null) {
                     clearProfileProto()
+                    return@copy
+                }
+
+                profileProto = profileProto {
+                    avatarUrl = profile.avatarUrl.orEmpty()
+                    emailAddress = profile.emailAddress
+                    displayName = profile.displayName
                 }
             }
         }
     }
 }
 
-/**
- * Based on Dependency Rule in Clean Architecture inner circle nothing know about outer circle
- * Thus DataSource aware to Domain Models but
- * Repository not access to instantiation Dto Models
- */
 interface UserPreferencesLocalDataSource {
     val userPreferences: Flow<UserPreferences>
     suspend fun setThemeMode(mode: ThemeMode)
