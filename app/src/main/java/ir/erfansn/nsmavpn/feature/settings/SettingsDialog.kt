f9364de3b2dce9dcb01c08ed7ba896360f802a5f
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-03-07 14:16:56 +0330

    Refactor SettingsDialog

--- app/src/main/java/ir/erfansn/nsmavpn/feature/settings/SettingsDialog.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/feature/settings/SettingsDialog.kt
@@ -1,18 +1,26 @@
 package ir.erfansn.nsmavpn.feature.settings
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ir.erfansn.nsmavpn.BuildConfig
@@ -44,7 +52,7 @@ private fun SettingsDialog(
     AlertDialog(
         onDismissRequest = onDismiss,
         title = {
-            Text(text = stringResource(id = R.string.settings))
+            Text(text = stringResource(id = R.string.title_settings))
         },
         text = {
             Divider()
@@ -53,34 +61,22 @@ private fun SettingsDialog(
                     .verticalScroll(rememberScrollState())
                     .wrapContentSize()
                     .padding(top = 16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+                verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
                 Text(
                     text = stringResource(R.string.theme_mode),
                     style = MaterialTheme.typography.titleMedium,
                 )
-                Column(modifier = Modifier.selectableGroup()) {
-                    SelectiveRow(
-                        text = stringResource(R.string.theme_mode_default),
-                        selected = uiState.themeMode == ThemeMode.SYSTEM,
-                        onClick = { onChangeThemeMode(ThemeMode.SYSTEM) }
-                    )
-                    SelectiveRow(
-                        text = stringResource(R.string.theme_mode_light),
-                        selected = uiState.themeMode == ThemeMode.LIGHT,
-                        onClick = { onChangeThemeMode(ThemeMode.LIGHT) }
-                    )
-                    SelectiveRow(
-                        text = stringResource(R.string.theme_mode_dark),
-                        selected = uiState.themeMode == ThemeMode.DARK,
-                        onClick = { onChangeThemeMode(ThemeMode.DARK) }
-                    )
-                }
+                ThemeModeConfig(
+                    themeMode = uiState.themeMode,
+                    onChangeThemeMode = onChangeThemeMode
+                )
                 Divider()
+                LinksPanel(
+                    modifier = Modifier.align(CenterHorizontally)
+                )
                 Text(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .padding(top = 8.dp),
+                    modifier = Modifier.align(CenterHorizontally),
                     text = stringResource(R.string.app_version, BuildConfig.VERSION_NAME)
                 )
             }
@@ -94,10 +90,35 @@ private fun SettingsDialog(
 }
 
 @Composable
+private fun ThemeModeConfig(
+    modifier: Modifier = Modifier,
+    themeMode: ThemeMode,
+    onChangeThemeMode: (mode: ThemeMode) -> Unit,
+) {
+    Column(modifier = modifier.selectableGroup()) {
+        SelectiveRow(
+            text = stringResource(R.string.theme_mode_default),
+            selected = themeMode == ThemeMode.SYSTEM,
+            onSelect = { onChangeThemeMode(ThemeMode.SYSTEM) }
+        )
+        SelectiveRow(
+            text = stringResource(R.string.theme_mode_light),
+            selected = themeMode == ThemeMode.LIGHT,
+            onSelect = { onChangeThemeMode(ThemeMode.LIGHT) }
+        )
+        SelectiveRow(
+            text = stringResource(R.string.theme_mode_dark),
+            selected = themeMode == ThemeMode.DARK,
+            onSelect = { onChangeThemeMode(ThemeMode.DARK) }
+        )
+    }
+}
+
+@Composable
 private fun SelectiveRow(
     text: String,
     selected: Boolean,
-    onClick: () -> Unit,
+    onSelect: () -> Unit,
 ) {
     Row(
         Modifier
@@ -105,7 +126,7 @@ private fun SelectiveRow(
             .selectable(
                 selected = selected,
                 role = Role.RadioButton,
-                onClick = onClick,
+                onClick = onSelect,
             )
             .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
@@ -117,6 +138,45 @@ private fun SelectiveRow(
         Spacer(modifier = Modifier.width(8.dp))
         Text(text)
     }
+}
+
+@Composable
+fun LinksPanel(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        TextLink(
+            text = stringResource(R.string.feedback),
+            url = stringResource(R.string.link_feedback)
+        )
+        TextLink(
+            text = stringResource(R.string.licences),
+            url = stringResource(R.string.link_licences)
+        )
+    }
+}
+
+@Composable
+fun TextLink(text: String, url: String) {
+    val launchResourceIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    val context = LocalContext.current
+
+    ClickableText(
+        text = AnnotatedString(
+            text = text,
+            spanStyle = SpanStyle(
+                color = MaterialTheme.colorScheme.primary,
+            )
+        ),
+        style = MaterialTheme.typography.labelLarge,
+        onClick = {
+            ActivityCompat.startActivity(context, launchResourceIntent, null)
+        }
+    )
 }
 
 @ThemePreviews
