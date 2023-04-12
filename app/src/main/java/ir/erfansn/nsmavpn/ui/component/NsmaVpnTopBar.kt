25f7e5f7f67dab3954b50a189a056b7dcf4cab5b
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-04-12 23:52:10 +0330

    Add NsmaVpnTopBar

---
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/component/NsmaVpnTopBar.kt
@@ -0,0 +1,64 @@
+package ir.erfansn.nsmavpn.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ir.erfansn.nsmavpn.ui.theme.NsmaVpnTheme
+import ir.erfansn.nsmavpn.ui.util.preview.ThemePreviews
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NsmaVpnTopBar(
+    title: String,
+    navigationIcon: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = { Text(text = title) },
+        navigationIcon = navigationIcon,
+        actions = actions,
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            // For transform color without flicker from transparent to target color
+            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0f),
+            scrolledContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+        ),
+        windowInsets = WindowInsets.safeContent.only(
+            WindowInsetsSides.Top + WindowInsetsSides.Horizontal
+        ),
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+fun NsmaVpnTopBarPreview() {
+    NsmaVpnTheme {
+        NsmaVpnTopBar(
+            title = "Test",
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Rounded.AccountCircle,
+                        contentDescription = null,
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Clear,
+                        contentDescription = null,
+                    )
+                }
+            }
+        )
+    }
+}
