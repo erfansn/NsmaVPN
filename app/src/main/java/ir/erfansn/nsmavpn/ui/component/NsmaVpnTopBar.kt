f56867a9a2bedfcb2a495f3dff4ae4273edb4ffd
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-09-07 14:49:56 +0330

    Add NsmaVpnLargeTopBar and able to change its color

--- app/src/main/java/ir/erfansn/nsmavpn/ui/component/NsmaVpnTopBar.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/component/NsmaVpnTopBar.kt
@@ -2,13 +2,13 @@ package ir.erfansn.nsmavpn.ui.component
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import ir.erfansn.nsmavpn.R
 import ir.erfansn.nsmavpn.ui.theme.NsmaVpnTheme
 import ir.erfansn.nsmavpn.ui.util.preview.ThemePreviews
 
@@ -20,6 +20,10 @@ fun NsmaVpnTopBar(
     actions: @Composable RowScope.() -> Unit,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        containerColor = scrollBehavior.containerColor,
+        scrolledContainerColor = scrolledContainerColor
+    )
 ) {
     CenterAlignedTopAppBar(
         modifier = modifier,
@@ -27,16 +31,45 @@ fun NsmaVpnTopBar(
         navigationIcon = navigationIcon,
         actions = actions,
         scrollBehavior = scrollBehavior,
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            // For transform color without flicker from transparent to target color
-            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0f),
-            scrolledContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
-        ),
-        windowInsets = WindowInsets.safeContent.only(
-            WindowInsetsSides.Top + WindowInsetsSides.Horizontal
-        ),
+        colors = colors,
+        windowInsets = windowInsets,
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NsmaVpnLargeTopBar(
+    title: @Composable () -> Unit,
+    navigationIcon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        containerColor = scrollBehavior.containerColor,
+        scrolledContainerColor = scrolledContainerColor,
+    )
+) {
+    LargeTopAppBar(
+        modifier = modifier,
+        title = title,
+        navigationIcon = navigationIcon,
+        scrollBehavior = scrollBehavior,
+        colors = colors,
+        windowInsets = windowInsets,
+    )
+}
+
+// For transform color without flicker from transparent to target color
+@OptIn(ExperimentalMaterial3Api::class)
+val TopAppBarScrollBehavior?.containerColor: Color
+    @Composable
+    get() = MaterialTheme.colorScheme.surface.copy(alpha = if (this == null) 1f else 0f)
+val scrolledContainerColor @Composable
+    get() = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+
+private val windowInsets @Composable
+    get() = WindowInsets.safeContent.only(
+        WindowInsetsSides.Top + WindowInsetsSides.Horizontal
+    )
 
 @OptIn(ExperimentalMaterial3Api::class)
 @ThemePreviews
@@ -63,6 +96,27 @@ fun NsmaVpnTopBarPreview() {
                     )
                 }
             }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+fun NsmaVpnLargeTopBarPreview() {
+    NsmaVpnTheme {
+        NsmaVpnLargeTopBar(
+            title = {
+                Text(text = "Test")
+            },
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                        contentDescription = null,
+                    )
+                }
+            },
         )
     }
 }
