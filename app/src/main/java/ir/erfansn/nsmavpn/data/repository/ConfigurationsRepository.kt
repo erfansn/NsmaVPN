164df692b7ae7e785aa9c121ad26b0aa6afc20b7
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-09-07 14:20:31 +0330

    Add functionalities manipulate splitting tunnel list and enable dynamic scheme

--- app/src/main/java/ir/erfansn/nsmavpn/data/repository/ConfigurationsRepository.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/repository/ConfigurationsRepository.kt
@@ -1,7 +1,9 @@
 package ir.erfansn.nsmavpn.data.repository
 
-import ir.erfansn.nsmavpn.data.model.ThemeMode
+import ir.erfansn.nsmavpn.data.model.AppInfo
+import ir.erfansn.nsmavpn.data.model.Configurations
 import ir.erfansn.nsmavpn.data.source.local.UserPreferencesLocalDataSource
+import ir.erfansn.nsmavpn.data.source.local.datastore.UserPreferences
 import ir.erfansn.nsmavpn.data.source.local.datastore.model.toConfigurations
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -9,11 +11,27 @@ import javax.inject.Inject
 class ConfigurationsRepository @Inject constructor(
     private val userPreferencesLocalDataSource: UserPreferencesLocalDataSource,
 ) {
-    val configurations = userPreferencesLocalDataSource.userPreferences.map {
-        it.toConfigurations()
+    val configurations = userPreferencesLocalDataSource
+        .userPreferences
+        .map(UserPreferences::toConfigurations)
+
+    suspend fun setThemeMode(mode: Configurations.ThemeMode) {
+        userPreferencesLocalDataSource.setThemeMode(mode)
     }
 
-    suspend fun setThemeMode(mode: ThemeMode) {
-        userPreferencesLocalDataSource.setThemeMode(mode)
+    suspend fun addAppToSplitTunnelingList(vararg apps: AppInfo) {
+        userPreferencesLocalDataSource.addAppsToSplitTunnelingList(apps.toList())
+    }
+
+    suspend fun removeAppFromSplitTunnelingList(app: AppInfo) {
+        userPreferencesLocalDataSource.removeAppFromSplitTunnelingList(app)
+    }
+
+    suspend fun removeAllAppsFromSplitTunnelingList() {
+        userPreferencesLocalDataSource.clearSplitTunnelingList()
+    }
+
+    suspend fun setDynamicSchemeEnable(enable: Boolean) {
+        userPreferencesLocalDataSource.setDynamicSchemeEnable(enable)
     }
 }
