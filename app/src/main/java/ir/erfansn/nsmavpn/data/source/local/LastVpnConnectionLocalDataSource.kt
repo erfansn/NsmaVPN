d30ad45efa53b28a3a51612c9ba16d71a7b499d5
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-09-22 13:26:09 +0330

    Expose flow of LastVpnConnection

--- app/src/main/java/ir/erfansn/nsmavpn/data/source/local/LastVpnConnectionLocalDataSource.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/source/local/LastVpnConnectionLocalDataSource.kt
@@ -4,11 +4,14 @@ import androidx.datastore.core.DataStore
 import ir.erfansn.nsmavpn.data.source.local.datastore.LastVpnConnection
 import ir.erfansn.nsmavpn.data.source.local.datastore.Server
 import ir.erfansn.nsmavpn.data.source.local.datastore.copy
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class DefaultLastVpnConnectionLocalDataSource @Inject constructor(
     private val dataStore: DataStore<LastVpnConnection>
 ) : LastVpnConnectionLocalDataSource {
+
+    override val lastVpnConnection: Flow<LastVpnConnection> = dataStore.data
 
     override suspend fun saveLastConnectionInfo(vpnServer: Server, connectionTimeSinceEpoch: Long) {
         dataStore.updateData {
@@ -21,5 +24,6 @@ class DefaultLastVpnConnectionLocalDataSource @Inject constructor(
 }
 
 interface LastVpnConnectionLocalDataSource {
+    val lastVpnConnection: Flow<LastVpnConnection>
     suspend fun saveLastConnectionInfo(vpnServer: Server, connectionTimeSinceEpoch: Long)
 }
