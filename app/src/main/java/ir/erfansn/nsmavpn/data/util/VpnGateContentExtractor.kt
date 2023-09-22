d6f4990e00eaa191520aeeeb4e5cf701857193b0
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-09-22 11:34:32 +0330

    Refactor and change to extract country code instead of country name

--- app/src/main/java/ir/erfansn/nsmavpn/data/util/VpnGateContentExtractor.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/util/VpnGateContentExtractor.kt
@@ -2,13 +2,16 @@ package ir.erfansn.nsmavpn.data.util
 
 import ir.erfansn.nsmavpn.data.source.local.datastore.Protocol
 import ir.erfansn.nsmavpn.data.source.local.datastore.Server
-import ir.erfansn.nsmavpn.data.source.local.datastore.UrlLink
+import ir.erfansn.nsmavpn.data.source.local.datastore.server
+import ir.erfansn.nsmavpn.data.source.local.datastore.urlLink
 import ir.erfansn.nsmavpn.di.IoDispatcher
 import it.skrape.core.htmlDocument
 import it.skrape.fetcher.AsyncFetcher
 import it.skrape.fetcher.response
 import it.skrape.fetcher.skrape
+import it.skrape.selects.Doc
 import it.skrape.selects.eachText
+import it.skrape.selects.html5.img
 import it.skrape.selects.html5.td
 import it.skrape.selects.html5.tr
 import kotlinx.coroutines.CoroutineDispatcher
@@ -19,65 +22,73 @@ class DefaultVpnGateContentExtractor @Inject constructor(
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : VpnGateContentExtractor {
 
-    override suspend fun extractSstpVpnServers(address: String) = withContext(ioDispatcher) {
+    override suspend fun extractSstpVpnServers(vpnGateUrl: String) = withContext(ioDispatcher) {
         skrape(AsyncFetcher) {
-            request { url = address }
+            request {
+                url = vpnGateUrl
+                timeout = 10_000
+            }
             response {
                 htmlDocument {
-                    val countrySelector = tr {
-                        td(":first-child") {
-                            withAttribute = "style" to "text-align: center;"
+                    countryFlagImageUrlSelector.zip(serverUrlSelector)
+                        .filter { (_, serverUrl) -> serverUrl.isNotEmpty() }
+                        .map { (countryFlagUrl, serverUrl) ->
+                            val urlParts = serverUrl.split(":")
 
-                            findAll { eachText }
-                        }
-                    }
-                    val serverSelector = tr {
-                        td(":nth-child(8)") {
-                            withAttribute =
-                                "style" to "text-align: center; word-break: break-all; white-space: normal;"
-
-                            findAll {
-                                eachText.map { it.substringAfter(":").trim() }
+                            server {
+                                countryCode = countryFlagUrl.substringAfterLast("/").split(".")[0]
+                                address = urlLink {
+                                    protocol = Protocol.HTTPS
+                                    hostName = urlParts.first()
+                                    portNumber = urlParts.getOrNull(1)?.toInt() ?: 443
+                                }
                             }
-                        }
-                    }
-
-                    countrySelector.zip(serverSelector).filterNot { it.second.isEmpty() }
-                        .map { (country, url) ->
-                            Server.newBuilder()
-                                .setCountryCode(country)
-                                .setAddress(url.toUrlLink())
-                                .build()
                         }
                 }
             }
         }
     }
 
-    private fun String.toUrlLink(): UrlLink {
-        val urlParts = split(":")
-        return UrlLink.newBuilder()
-            .setProtocol(Protocol.HTTPS)
-            .setHostName(urlParts.first())
-            .setPortNumber(urlParts.getOrNull(1)?.toInt() ?: 443)
-            .build()
-    }
+    private val Doc.countryFlagImageUrlSelector
+        get() = tr {
+            td(":first-child") {
+                withAttribute = "style" to "text-align: center;"
+
+                img {
+                    findAll {
+                        map { it attribute "src" }
+                    }
+                }
+            }
+        }
+
+    private val Doc.serverUrlSelector
+        get() = tr {
+            td(":nth-child(8)") {
+                withAttribute =
+                    "style" to "text-align: center; word-break: break-all; white-space: normal;"
+
+                "p span b span" {
+                    "style" to "color: #006600;"
+
+                    findAll { eachText }
+                }
+            }
+        }
 
     override fun findVpnGateMirrorLinks(content: String) = VPN_GATE_MIRROR_LINK_REGEX
         .findAll(content)
-        .map {
-            val (protocol, host, port) = it.destructured
-            MirrorLink(protocol, host, port)
-        }
+        .map(MatchResult::destructured)
+        .map { (protocol, host, port) -> MirrorLink(protocol, host, port) }
         .toList()
 
     companion object {
-        val VPN_GATE_MIRROR_LINK_REGEX = """(\w+)://(.*):(\d{1,5})""".toRegex()
+        private val VPN_GATE_MIRROR_LINK_REGEX = """(\w+)://(.*):(\d{1,5})""".toRegex()
     }
 }
 
 interface VpnGateContentExtractor {
-    suspend fun extractSstpVpnServers(address: String): List<Server>
+    suspend fun extractSstpVpnServers(vpnGateUrl: String): List<Server>
     fun findVpnGateMirrorLinks(content: String): List<MirrorLink>
 }
 
@@ -90,9 +101,3 @@ data class MirrorLink(
         return "$protocol://$hostName:$port"
     }
 }
-
-fun MirrorLink.asUrlLink(): UrlLink = UrlLink.newBuilder()
-    .setProtocolValue(if (protocol == "http") 0 else 1)
-    .setHostName(hostName)
-    .setPortNumber(port.toInt())
-    .build()
