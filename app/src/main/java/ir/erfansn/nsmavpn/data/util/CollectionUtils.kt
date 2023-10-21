0f2a94bfd33e0c0f6480bc8772c6449030c88ff7
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-10-21 18:49:58 +0330

    Refactor utility functions

--- app/src/main/java/ir/erfansn/nsmavpn/data/util/CollectionUtils.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/util/CollectionUtils.kt
@@ -1,65 +1,79 @@
 package ir.erfansn.nsmavpn.data.util
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 import kotlin.coroutines.CoroutineContext
 
-suspend fun <T, R> T.runWithContext(coroutineContext: CoroutineContext, block: suspend T.() -> R): R {
+suspend fun <T, R> T.runWithContext(
+    coroutineContext: CoroutineContext,
+    block: suspend T.() -> R,
+): R {
     return withContext(coroutineContext) { block() }
 }
 
-suspend inline fun <T, R : Comparable<R>> Iterable<T>.asyncMinByOrNull(
-    crossinline selector: suspend (T) -> R,
-): T? = coroutineScope {
-    val result = map {
-        async {
-            selector(it) to it
-        }
-    }.awaitAll()
-
-    result.minByOrNull { it.first }?.second
-}
+suspend inline fun <T, R : Comparable<R>> Iterable<T>.asyncMinByOrNull(crossinline selector: suspend (T) -> R) =
+    asyncMap {
+        selector(it) to it
+    }.minByOrNull {
+        it.first
+    }?.second
 
 suspend inline fun <T> Iterable<T>.asyncPartition(
     crossinline selector: suspend (T) -> Boolean,
-): Pair<List<T>, List<T>> = coroutineScope {
-    val result = map {
-        async {
-            selector(it) to it
-        }
-    }.awaitAll()
-
+): Pair<List<T>, List<T>> {
     val first = ArrayList<T>()
     val second = ArrayList<T>()
-    for (element in result) {
-        if (element.first) {
-            first.add(element.second)
+
+    asyncMap {
+        selector(it) to it
+    }.fastForEach { (predicate, value) ->
+        if (predicate) {
+            first.add(value)
         } else {
-            second.add(element.second)
+            second.add(value)
         }
     }
-    Pair(first, second)
+    return Pair(first, second)
 }
 
-suspend inline fun <T> Iterable<T>.asyncFilterNotTo(
-    crossinline predicate: suspend (T) -> Boolean
-) = coroutineScope {
-    val result = map {
-        async {
-            predicate(it) to it
-        }
-    }.awaitAll()
-
-    result.filterNot { it.first }.map { it.second }
+/**
+ * Use this for walk over random-access collections
+ * it's free from extra allocation of Iteration objects thus provides better performance.
+ *
+ * [See this video](https://youtu.be/MZOf3pOAM6A?si=C2GdYPiCCW38eoqU)
+ **/
+@OptIn(ExperimentalContracts::class)
+inline fun <T> List<T>.fastForEach(action: (T) -> Unit) {
+    contract { callsInPlace(action) }
+    for (index in indices) action(this[index])
 }
 
-suspend inline fun <T> Iterable<T>.asyncFirstOrNull(
-    crossinline predicate: suspend (T) -> Boolean
-): T? = coroutineScope {
-    val result = map {
+suspend inline fun <T> Iterable<T>.asyncFilterNotTo(crossinline predicate: suspend (T) -> Boolean) =
+    asyncMap {
+        predicate(it) to it
+    }.filter {
+        it.first
+    }.map {
+        it.second
+    }
+
+suspend inline fun <T> Iterable<T>.asyncFirstOrNull(crossinline predicate: suspend (T) -> Boolean) =
+    asyncMap {
+        predicate(it) to it
+    }.firstOrNull {
+        it.first
+    }?.second
+
+suspend inline fun <T, R> Iterable<T>.asyncMap(
+    crossinline block: suspend (T) -> R,
+): List<R> = coroutineScope {
+    map {
         async {
-            predicate(it) to it
+            block(it)
         }
     }.awaitAll()
-
-    result.firstOrNull { it.first }?.second
 }
