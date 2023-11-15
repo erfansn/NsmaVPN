40a64f5b6f227ffa18a9114b641d885c879f8434
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-11-15 19:48:47 +0330

    Query the network usage stats every 1 sec

--- app/src/main/java/ir/erfansn/nsmavpn/data/util/NetworkUsageTracker.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/util/NetworkUsageTracker.kt
@@ -1,17 +1,14 @@
 package ir.erfansn.nsmavpn.data.util
 
-import android.app.AppOpsManager
-import android.app.AppOpsManager.MODE_ALLOWED
-import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.app.usage.NetworkStats.Bucket
 import android.app.usage.NetworkStatsManager
 import android.content.Context
 import android.net.ConnectivityManager
-import android.os.Build
-import android.os.Process
 import androidx.core.content.getSystemService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import ir.erfansn.nsmavpn.data.model.NetworkUsage
 import ir.erfansn.nsmavpn.di.IoDispatcher
+import ir.erfansn.nsmavpn.feature.home.util.isGrantedGetUsageStatsPermission
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -21,24 +18,17 @@ import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
 
 class DefaultNetworkUsageTracker @Inject constructor(
-    @ApplicationContext context: Context,
+    @ApplicationContext private val context: Context,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : NetworkUsageTracker {
-
-    init {
-        require(context.isGrantedGetUsageStatsPermission)
-    }
 
     private val networkStatsManager = context.getSystemService<NetworkStatsManager>()!!
 
     override fun trackUsage(startEpochTime: Long): Flow<NetworkUsage> =
         flow {
-            var wifiStatsBucket = queryWifiStatsBucket(startEpochTime)
-            var mobileStatsBucket = queryMobileStatsBucket(startEpochTime)
-
             while (true) {
-                wifiStatsBucket = queryWifiStatsBucket(startEpochTime) ?: wifiStatsBucket
-                mobileStatsBucket = queryMobileStatsBucket(startEpochTime) ?: mobileStatsBucket
+                val wifiStatsBucket = queryWifiStatsBucket(startEpochTime)
+                val mobileStatsBucket = queryMobileStatsBucket(startEpochTime)
 
                 emit(
                     value = NetworkUsage(
@@ -46,36 +36,35 @@ class DefaultNetworkUsageTracker @Inject constructor(
                         transmitted = wifiStatsBucket.txBytes + mobileStatsBucket.txBytes,
                     )
                 )
-                delay(3.seconds)
+                delay(1.seconds)
             }
         }.flowOn(ioDispatcher)
 
-    private fun queryWifiStatsBucket(startEpochTime: Long) = networkStatsManager.querySummaryForDevice(
-        ConnectivityManager.TYPE_WIFI,
-        null,
-        startEpochTime,
-        Long.MAX_VALUE,
+    private fun queryWifiStatsBucket(startEpochTime: Long) = queryStatsBucket(
+        networkType = ConnectivityManager.TYPE_WIFI,
+        startEpochTime = startEpochTime,
     )
 
-    private fun queryMobileStatsBucket(startEpochTime: Long) = networkStatsManager.querySummaryForDevice(
-        ConnectivityManager.TYPE_MOBILE,
-        null,
-        startEpochTime,
-        Long.MAX_VALUE,
+    private fun queryMobileStatsBucket(startEpochTime: Long) = queryStatsBucket(
+        networkType = ConnectivityManager.TYPE_MOBILE,
+        startEpochTime = startEpochTime,
     )
+
+    private fun queryStatsBucket(
+        networkType: Int,
+        startEpochTime: Long,
+    ): Bucket {
+        check(context.isGrantedGetUsageStatsPermission)
+
+        return networkStatsManager.querySummaryForDevice(
+            networkType,
+            null,
+            startEpochTime,
+            Long.MAX_VALUE,
+        ) ?: Bucket()
+    }
 }
 
 interface NetworkUsageTracker {
     fun trackUsage(startEpochTime: Long): Flow<NetworkUsage>
 }
-
-val Context.isGrantedGetUsageStatsPermission: Boolean
-    get() {
-        val appOps = getSystemService<AppOpsManager>()!!
-        val mode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            appOps.unsafeCheckOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
-        } else {
-            appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
-        }
-        return mode == MODE_ALLOWED
-    }
