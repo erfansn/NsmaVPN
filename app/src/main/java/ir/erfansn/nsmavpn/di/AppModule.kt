91c19c7e1d052571c97d2a09f617d6889b988133
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-11-16 20:19:20 +0330

    Remove VpnServiceState related classes

--- app/src/main/java/ir/erfansn/nsmavpn/di/AppModule.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/di/AppModule.kt
@@ -1,14 +1,14 @@
 package ir.erfansn.nsmavpn.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import androidx.work.WorkManager
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.gmail.Gmail
-import com.google.api.services.gmail.GmailScopes
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -17,24 +17,22 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import ir.erfansn.nsmavpn.data.repository.DefaultLastVpnConnectionRepository
 import ir.erfansn.nsmavpn.data.repository.DefaultVpnGateMailRepository
-import ir.erfansn.nsmavpn.data.repository.DefaultVpnServiceStateRepository
 import ir.erfansn.nsmavpn.data.repository.LastVpnConnectionRepository
 import ir.erfansn.nsmavpn.data.repository.VpnGateMailRepository
-import ir.erfansn.nsmavpn.data.repository.VpnServiceStateRepository
 import ir.erfansn.nsmavpn.data.source.local.DefaultLastVpnConnectionLocalDataSource
 import ir.erfansn.nsmavpn.data.util.DefaultInstalledAppsListProvider
 import ir.erfansn.nsmavpn.data.util.InstalledAppsListProvider
 import ir.erfansn.nsmavpn.data.source.local.DefaultUserPreferencesLocalDataSource
 import ir.erfansn.nsmavpn.data.source.local.DefaultVpnProviderLocalDataSource
-import ir.erfansn.nsmavpn.data.source.local.DefaultVpnServiceStateLocalDataSource
 import ir.erfansn.nsmavpn.data.source.local.LastVpnConnectionLocalDataSource
 import ir.erfansn.nsmavpn.data.source.local.UserPreferencesLocalDataSource
 import ir.erfansn.nsmavpn.data.source.local.VpnProviderLocalDataSource
-import ir.erfansn.nsmavpn.data.source.local.VpnServiceStateLocalDataSource
+import ir.erfansn.nsmavpn.data.source.local.datastore.LastVpnConnection
 import ir.erfansn.nsmavpn.data.source.local.datastore.LastVpnConnectionSerializer
+import ir.erfansn.nsmavpn.data.source.local.datastore.UserPreferences
 import ir.erfansn.nsmavpn.data.source.local.datastore.UserPreferencesSerializer
+import ir.erfansn.nsmavpn.data.source.local.datastore.VpnProvider
 import ir.erfansn.nsmavpn.data.source.local.datastore.VpnProviderSerializer
-import ir.erfansn.nsmavpn.data.source.local.datastore.VpnServiceStateSerializer
 import ir.erfansn.nsmavpn.data.source.remote.DefaultVpnGateMessagesRemoteDataSource
 import ir.erfansn.nsmavpn.data.source.remote.VpnGateMessagesRemoteDataSource
 import ir.erfansn.nsmavpn.data.source.remote.api.GmailApi
@@ -116,73 +114,52 @@ interface AppModule {
         connectivityNetworkMonitor: ConnectivityNetworkMonitor,
     ): NetworkMonitor
 
-    @Binds
-    fun bindsVpnServiceStateLocalDataSource(
-        defaultVpnServiceStateLocalDataSource: DefaultVpnServiceStateLocalDataSource
-    ): VpnServiceStateLocalDataSource
-
-    @Binds
-    fun bindsVpnServiceStateRepository(
-        defaultVpnServiceStateRepository: DefaultVpnServiceStateRepository
-    ): VpnServiceStateRepository
-
     companion object {
 
         @[IoDispatcher Provides]
-        fun providesIoDispatcher() = Dispatchers.IO
+        fun providesIoDispatcher(): CoroutineDispatcher =
+            Dispatchers.IO
 
         @[DefaultDispatcher Provides]
-        fun providesDefaultDispatcher() = Dispatchers.Default
+        fun providesDefaultDispatcher(): CoroutineDispatcher =
+            Dispatchers.Default
 
         @Provides
-        fun providesApplicationCoroutineScope(
-            @IoDispatcher ioDispatcher: CoroutineDispatcher,
-        ) = CoroutineScope(SupervisorJob() + ioDispatcher)
+        fun providesApplicationCoroutineScope(@IoDispatcher ioDispatcher: CoroutineDispatcher): CoroutineScope =
+            CoroutineScope(SupervisorJob() + ioDispatcher)
 
         @[Provides Singleton]
-        fun providesUserPreferencesDataStore(@ApplicationContext context: Context) =
+        fun providesUserPreferencesDataStore(@ApplicationContext context: Context): DataStore<UserPreferences> =
             DataStoreFactory.create(
                 serializer = UserPreferencesSerializer,
                 produceFile = { context.dataStoreFile("user_preferences") }
             )
 
         @[Provides Singleton]
-        fun providesVpnProviderDataStore(@ApplicationContext context: Context) =
+        fun providesVpnProviderDataStore(@ApplicationContext context: Context): DataStore<VpnProvider> =
             DataStoreFactory.create(
                 serializer = VpnProviderSerializer,
                 produceFile = { context.dataStoreFile("vpn_provider") }
             )
 
         @[Provides Singleton]
-        fun providesLastVpnConnectionDataStore(@ApplicationContext context: Context) =
+        fun providesLastVpnConnectionDataStore(@ApplicationContext context: Context): DataStore<LastVpnConnection> =
             DataStoreFactory.create(
                 serializer = LastVpnConnectionSerializer,
                 produceFile = { context.dataStoreFile("last_vpn_connection") }
             )
 
-        @[Provides Singleton]
-        fun providesVpnServiceStateDataStore(@ApplicationContext context: Context) =
-            DataStoreFactory.create(
-                serializer = VpnServiceStateSerializer,
-                produceFile = { context.dataStoreFile("vpn_service_state") }
-            )
-
         @Provides
-        fun providesWorkManager(@ApplicationContext context: Context) =
+        fun providesWorkManager(@ApplicationContext context: Context): WorkManager =
             WorkManager.getInstance(context)
 
         @[Provides Singleton]
-        fun providesGoogleAccountCredential(@ApplicationContext context: Context) =
-            GoogleAccountCredential.usingOAuth2(
-                context,
-                listOf(GmailScopes.GMAIL_READONLY)
-            )!!
-
-        @[Provides Singleton]
-        fun providesNetHttpTransport() = GoogleNetHttpTransport.newTrustedTransport()!!
+        fun providesNetHttpTransport(): NetHttpTransport =
+            GoogleNetHttpTransport.newTrustedTransport()
 
         @Provides
-        fun providesGsonFactory() = GsonFactory.getDefaultInstance()!!
+        fun providesGsonFactory(): GsonFactory =
+            GsonFactory.getDefaultInstance()
     }
 }
 
