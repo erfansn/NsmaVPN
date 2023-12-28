b16a1df053d03cc501c172bfcee7bb6a35327f2e
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-12-28 20:46:46 +0330

    Provide a instance of GoogleAccountCredential directly

--- app/src/main/java/ir/erfansn/nsmavpn/data/source/remote/api/GmailApi.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/source/remote/api/GmailApi.kt
@@ -1,7 +1,9 @@
 package ir.erfansn.nsmavpn.data.source.remote.api
 
 import android.content.Context
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.services.gmail.Gmail
+import com.google.api.services.gmail.GmailScopes
 import dagger.hilt.android.qualifiers.ApplicationContext
 import ir.erfansn.nsmavpn.R
 import javax.inject.Inject
@@ -9,6 +11,11 @@ import javax.inject.Inject
 class GmailApi @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : GoogleApi<Gmail>() {
+
+    override val credential: GoogleAccountCredential = GoogleAccountCredential.usingOAuth2(
+        context,
+        listOf(GmailScopes.GMAIL_READONLY),
+    )
 
     override fun getService(): Gmail =
         Gmail.Builder(httpTransport, gsonFactory, credential)
