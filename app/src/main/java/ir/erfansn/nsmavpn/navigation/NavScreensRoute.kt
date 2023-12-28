d030d02ffe593c4092809296c3a5d917df45fa95
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2023-12-28 20:43:00 +0330

    Add NavScreensRoute

---
+++ app/src/main/java/ir/erfansn/nsmavpn/navigation/NavScreensRoute.kt
@@ -0,0 +1,8 @@
+package ir.erfansn.nsmavpn.navigation
+
+object NavScreensRoute {
+    const val Home = "home"
+    const val Auth = "auth"
+    const val Profile = "profile"
+    const val Settings = "settings"
+}
