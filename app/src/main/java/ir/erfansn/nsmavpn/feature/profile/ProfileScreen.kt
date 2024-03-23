0f642446f09307920b28824a2adc7c37db8c076a
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-03-23 10:47:47 +0330

    Correct user info field text direction according to its content

--- app/src/main/java/ir/erfansn/nsmavpn/feature/profile/ProfileScreen.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/feature/profile/ProfileScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -44,6 +45,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -282,7 +284,10 @@ private fun UserInfoField(
         value = value,
         onValueChange = { },
         readOnly = true,
-        singleLine = true
+        singleLine = true,
+        textStyle = LocalTextStyle.current.copy(
+            textDirection = TextDirection.Content
+        )
     )
 }
 
