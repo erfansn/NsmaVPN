67ff3d7565f802e3d2595843e38e811ad3c4961d
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-03-25 12:37:52 +0330

    Expose user profile data as stream

--- app/src/main/java/ir/erfansn/nsmavpn/data/source/local/UserProfileLocalDataSource.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/data/source/local/UserProfileLocalDataSource.kt
@@ -4,9 +4,11 @@ import androidx.datastore.core.DataStore
 import ir.erfansn.nsmavpn.data.model.Profile
 import ir.erfansn.nsmavpn.data.source.local.datastore.UserProfile
 import ir.erfansn.nsmavpn.data.source.local.datastore.copy
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 interface UserProfileLocalDataSource {
+    val userProfile: Flow<UserProfile>
     suspend fun saveUserProfile(profile: Profile)
     suspend fun clearUserProfile()
 }
@@ -14,6 +16,8 @@ interface UserProfileLocalDataSource {
 class DefaultUserProfileLocalDataSource @Inject constructor(
     private val dataStore: DataStore<UserProfile>
 ) : UserProfileLocalDataSource {
+
+    override val userProfile: Flow<UserProfile> = dataStore.data
 
     override suspend fun saveUserProfile(profile: Profile) {
         dataStore.updateData {
