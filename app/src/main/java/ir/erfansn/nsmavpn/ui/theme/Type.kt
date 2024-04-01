5ad15af02b3a6ebfa52b900e1b7fabb5fca426aa
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-04-01 15:44:08 +0330

    Set Ballo Bhaijaan 2 as fontFamily

--- app/src/main/java/ir/erfansn/nsmavpn/ui/theme/Type.kt
+++ app/src/main/java/ir/erfansn/nsmavpn/ui/theme/Type.kt
@@ -1,9 +1,61 @@
 package ir.erfansn.nsmavpn.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import ir.erfansn.nsmavpn.R
 
-val Typography = Typography()
+val Typography = Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(
+            fontFamily = fontFamily
+        ),
+        displayMedium = displayMedium.copy(
+            fontFamily = fontFamily
+        ),
+        displaySmall = displaySmall.copy(
+            fontFamily = fontFamily
+        ),
+        headlineLarge = headlineLarge.copy(
+            fontFamily = fontFamily
+        ),
+        headlineMedium = headlineMedium.copy(
+            fontFamily = fontFamily
+        ),
+        headlineSmall = headlineSmall.copy(
+            fontFamily = fontFamily
+        ),
+        titleLarge = titleLarge.copy(
+            fontFamily = fontFamily
+        ),
+        titleMedium = titleMedium.copy(
+            fontFamily = fontFamily
+        ),
+        titleSmall = titleSmall.copy(
+            fontFamily = fontFamily
+        ),
+        bodyLarge = bodyLarge.copy(
+            fontFamily = fontFamily
+        ),
+        bodyMedium = bodyMedium.copy(
+            fontFamily = fontFamily
+        ),
+        bodySmall = bodySmall.copy(
+            fontFamily = fontFamily
+        ),
+        labelLarge = labelLarge.copy(
+            fontFamily = fontFamily
+        ),
+        labelMedium = labelMedium.copy(
+            fontFamily = fontFamily
+        ),
+        labelSmall = labelSmall.copy(
+            fontFamily = fontFamily
+        )
+    )
+}
+
+private val fontFamily = FontFamily(
+    Font(R.font.baloo_bhaijaan2, FontWeight.Normal)
+)
