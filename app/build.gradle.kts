d7f869daca9588347938df66141692ea78d090e1
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-04-30 10:50:49 +0330

    Enable resource shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
